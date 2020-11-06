55e9261d92be12162892dc3990a4c9974e7bd40a
Masayuki Morisaki <masayuki144@gmail.com>
2020-11-06 11:00:29 +0900

    表示/非表示、表示ロック機能を実装

--- src/components/atomos/Cell.tsx
+++ src/components/atomos/Cell.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
+import { useState } from 'react';
 
 const style = {
   cell: {
     display: 'inline-flex',
     width: 120,
     height: 120,
-    border: 'solid black 3px',
+    border: 'solid black 2px',
     justifyContent: 'center',
     alignItems: 'center',
     fontSize: '3rem'
   }
 }
 
-function Cell({ val }: { val: number }) {
+function Cell({ val, hidden = false, locked = false }: { val: number, hidden?: boolean, locked?: boolean }) {
+
+  const [isHidden, switchHidden] = useState(hidden)
+  const handleTap = () => locked ? null : switchHidden(!isHidden)
+
+
+  const divStyle = Object.assign(
+    { backgroundColor: !locked && isHidden ? 'gray' : 'white' },
+    style.cell
+  )
+
   return (
-    <div style={style.cell}>
-      <span> {val} </span>
+    <div style={divStyle} onTouchEnd={handleTap} onClick={handleTap}>
+      <span> {isHidden ? null : val} </span>
     </div>
   )
 }
