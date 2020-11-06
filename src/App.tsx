5ccd8295215018c63c862dfb98794c4c930ae75a
Masayuki Morisaki <masayuki144@gmail.com>
2020-11-06 11:15:32 +0900

    ディレクトリ移動

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './components/atomos/Cell'
+import './components/Cell'
 import Cell from './components/atomos/Cell';
 
 function App() {
