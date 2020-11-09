a2650d13cca6e8589bdc344f17992b9f0e1e9812
Masayuki Morisaki <masayuki144@gmail.com>
2020-11-09 13:22:01 +0900

    式リストを表示するコンポーネントを作成

--- src/enum.ts
+++ src/enum.ts
@@ -3,3 +3,39 @@ export enum MaskPosition {
   Right,
   Outcome
 }
+
+export enum MaskType {
+  Left,
+  Right,
+  Outcome,
+  Random
+}
+
+export function convertType(maskType?: MaskType | undefined): MaskPosition {
+  let position = (function (): MaskPosition {
+    if (maskType === MaskType.Random) {
+      let v = Math.floor((Math.random() * 10000)) % 3
+      switch (v) {
+        case 0:
+          return MaskPosition.Left
+        case 1:
+          return MaskPosition.Right
+        case 2:
+          return MaskPosition.Outcome
+      }
+      return MaskPosition.Outcome
+    }
+
+    switch (maskType) {
+      case MaskType.Left:
+        return MaskPosition.Left
+      case MaskType.Right:
+        return MaskPosition.Right
+      case MaskType.Outcome:
+        return MaskPosition.Outcome
+    }
+    return MaskPosition.Outcome
+  })()
+
+  return position
+}
