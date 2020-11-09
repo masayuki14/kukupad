52f7e88f609f28746f2d0404da5a85e32aef8e44
Masayuki Morisaki <masayuki144@gmail.com>
2020-11-09 10:12:09 +0900

    式表示のコンポーネントを作成

---
+++ src/components/Formula.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useState } from 'react';
+import Cell from './Cell'
+import { MaskPosition } from '../enum'
+
+type PropTypes = {
+  left: number;
+  right: number;
+  outcome: number;
+  mask?: MaskPosition
+}
+
+const style = {
+  display: 'flex',
+  justifyContent: 'center',
+  margin: 10
+}
+
+
+function Formula(props: PropTypes) {
+
+  return (
+    <div style={style}>
+      <Cell val={props.left} hidden={props.mask === MaskPosition.Left} locked={props.mask !== MaskPosition.Left} />
+      <Cell val="×" noBorder={true} slim={true} />
+      <Cell val={props.right} hidden={props.mask === MaskPosition.Right} locked={props.mask !== MaskPosition.Right} />
+      <Cell val="＝" noBorder={true} slim={true} />
+      <Cell val={props.outcome} hidden={props.mask === MaskPosition.Outcom} locked={props.mask !== MaskPosition.Outcom} />
+    </ div>
+  )
+}
+
+export default Formula
