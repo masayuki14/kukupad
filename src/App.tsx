d174ede8ede098a9ebccc74d78367c79d6fb7782
Masayuki Morisaki <masayuki144@gmail.com>
2020-11-10 10:28:51 +0900

    Themeを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,44 @@
 import React from 'react';
+import { createMuiTheme, Theme, ThemeProvider } from '@material-ui/core/styles'
 import logo from './logo.svg';
 import './App.css';
-import './components/Cell'
-import Cell from './components/Cell';
+import Formula from './components/Formula';
+import { MaskPosition, MaskType } from './enum'
+import List from './components/List'
+import Options from './components/Options'
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'light'
+  }
+})
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+      <ThemeProvider theme={theme}>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-      <body>
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <Cell val={5} locked={true} />
-          <Cell val={6} hidden={true} />
-          <Cell val={30} hidden={true} locked={true} />
-        </div>
-      </body>
+        </header>
+        <body>
+          <Options />
+          <Formula left={2} right={8} outcome={16} mask={MaskPosition.Left} />
+          <List axis={2} step={9} maskType={MaskType.Random} />
+          <List axis={13} step={16} />
+        </body>
+      </ThemeProvider>
     </div>
   );
 }
