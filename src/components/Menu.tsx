4b938107e3adec2a143b5e2d13ee31517370e40e
Masayuki Morisaki <masayuki144@gmail.com>
2020-11-11 11:40:20 +0900

    選択コンポーネントをまとめたMenu

---
+++ src/components/Menu.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Steps from './Steps'
+import Masks from './Masks'
+import Directions from './Directions'
+import Axes from './Axes'
+
+import { MultiplyStep, MaskType, SortDirection } from './../enum'
+
+type PropTypes = {
+  step?: MultiplyStep,
+  mask?: MaskType,
+  direction?: SortDirection
+  axis?: number,
+  onChangeStep?: (v: number) => void,
+  onChangeMask?: (v: number) => void,
+  onChangeDirection?: (v: number) => void,
+  onChangeAxis?: (v: number) => void
+}
+
+function Menu({
+  step,
+  mask,
+  direction,
+  axis,
+  onChangeStep,
+  onChangeMask,
+  onChangeDirection,
+  onChangeAxis
+}: PropTypes) {
+
+  return (
+    <div>
+      <Steps step={step} onChange={onChangeStep} />
+      <Masks mask={mask} onChange={onChangeMask} />
+      <Directions direction={direction} onChange={onChangeDirection} />
+      <Axes axis={axis} onChange={onChangeAxis} />
+    </div >
+  )
+}
+
+export default Menu
