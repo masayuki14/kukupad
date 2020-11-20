4668540cb51353deb63d82c4be26476002eb366c
Masayuki Morisaki <masayuki144@gmail.com>
2020-11-20 10:21:14 +0900

    かけるかずのばらばら表示に対応

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -5,24 +5,23 @@ import { MaskType, convertType } from './../enum'
 import { RubyTexts } from '../constants'
 
 type PropTypes = {
-  steps: number[]
-  axis: number
+  pairs?: { axis: number; step: number }[]
   mask?: MaskType
 }
 
 function List(props: PropTypes) {
-  const formulas = props.steps.map((v, i) => (
+  const formulas2 = props.pairs?.map((pair, i) => (
     <Grid item key={i} xs={12}>
       <Formula
         key={i}
-        left={props.axis}
-        right={v}
-        outcome={props.axis * v}
+        left={pair.axis}
+        right={pair.step}
+        outcome={pair.axis * pair.step}
         mask={convertType(props.mask)}
-        rtLeft={RubyTexts[props.axis]?.[v]?.[0]}
-        rtRight={RubyTexts[props.axis]?.[v]?.[1]}
-        rtEqual={RubyTexts[props.axis]?.[v]?.[2]}
-        rtOutcome={RubyTexts[props.axis]?.[v]?.[3]}
+        rtLeft={RubyTexts[pair.axis]?.[pair.step]?.[0]}
+        rtRight={RubyTexts[pair.axis]?.[pair.step]?.[1]}
+        rtEqual={RubyTexts[pair.axis]?.[pair.step]?.[2]}
+        rtOutcome={RubyTexts[pair.axis]?.[pair.step]?.[3]}
       />
     </Grid>
   ))
@@ -30,7 +29,7 @@ function List(props: PropTypes) {
   return (
     <React.Fragment>
       <Grid container direction="row" justify="flex-start" alignItems="center">
-        {formulas}
+        {formulas2}
       </Grid>
     </React.Fragment>
   )
